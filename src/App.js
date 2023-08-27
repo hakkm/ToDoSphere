05eb68e34f1aebaa0dc213921c79e130855024de
Khabir <hakim1khabir2@gmail.com>
2023-08-27 03:08:23 +0100

    edit button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const TODOS = [
   { id: 1, body: "Eat Healthy Food", active: false },
@@ -19,8 +19,14 @@ function AddBar({ onTodosChange, todos }) {
   return (
     <form onSubmit={handleSubmit}>
       <label>What Needs To Be Done?</label>
-      <input value={add} onChange={(e) => setAdd(e.target.value)}></input>
-      <button>Add</button>
+      <div>
+        <input
+          value={add}
+          onChange={(e) => setAdd(e.target.value)}
+          autoComplete="off"
+        ></input>
+        <button>Add</button>
+      </div>
     </form>
   );
 }
@@ -68,42 +74,84 @@ function Todos({ todos, filter, onTodosChange }) {
 
 function Todo({ todos, body, isActive, onTodosChange }) {
   const [complete, setComplete] = useState(isActive);
+  const [edit, setEdit] = useState(() => false);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    console.log("useEffect calling");
+    if (edit) inputRef.current.focus();
+  }, [edit]);
+  function handleSave(e) {
+    e.preventDefault();
+
+    const foundIndex = todos.findIndex((todo) => todo.body === body);
+    const updatedTodos = [...todos];
+
+    updatedTodos[foundIndex] = {
+      ...updatedTodos[foundIndex],
+      body: e.target.newName.value,
+    };
+
+    onTodosChange(updatedTodos);
+    setEdit(false);
+  }
   return (
     <>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={complete}
-            onChange={(e) => {
-              const foundIndex = todos.findIndex((todo) => todo.body === body);
-              const updatedTodos = [...todos];
+      {!edit ? (
+        <div>
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                checked={complete}
+                onChange={(e) => {
+                  const foundIndex = todos.findIndex(
+                    (todo) => todo.body === body
+                  );
+                  const updatedTodos = [...todos];
 
-              updatedTodos[foundIndex] = {
-                ...updatedTodos[foundIndex],
-                active: !complete,
-              };
+                  updatedTodos[foundIndex] = {
+                    ...updatedTodos[foundIndex],
+                    active: !complete,
+                  };
 
-              onTodosChange(updatedTodos);
-              setComplete(!complete);
-            }}
-          />{" "}
-          {body}
-        </label>
-      </div>
-      <div>
-        <button>Edit</button>
-        <button
-          onClick={() => {
-            const foundIndex = todos.findIndex((todo) => todo.body === body);
-            const deletedTodos = [...todos];
-            deletedTodos.splice(foundIndex, 1);
-            onTodosChange(deletedTodos);
-          }}
-        >
-          Delete
-        </button>
-      </div>
+                  onTodosChange(updatedTodos);
+                  setComplete(!complete);
+                }}
+              />{" "}
+              {body}
+            </label>
+          </div>
+          <div>
+            <button onClick={() => setEdit(true)}>Edit</button>
+            <button
+              onClick={() => {
+                const foundIndex = todos.findIndex(
+                  (todo) => todo.body === body
+                );
+                const deletedTodos = [...todos];
+                deletedTodos.splice(foundIndex, 1);
+                onTodosChange(deletedTodos);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ) : (
+        <form onSubmit={handleSave}>
+          <label>New name for {body}</label>
+          <div>
+            <input ref={inputRef} name="newName" autoComplete="off"></input>
+          </div>
+          <div>
+            <button type="button" onClick={() => setEdit(false)}>
+              Cancel
+            </button>
+            <button type="submit">Save</button>
+          </div>
+        </form>
+      )}
     </>
   );
 }
