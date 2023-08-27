b68466f9a94ee70fe99727e84407ee20c5494557
Khabir <hakim1khabir2@gmail.com>
2023-08-27 18:18:38 +0100

    clean Todo.js

--- src/Componenets/Todo.js
+++ src/Componenets/Todo.js
@@ -2,13 +2,13 @@ import { useState, useRef, useEffect } from "react";
 
 export default function Todo({ todos, body, isActive, onTodosChange }) {
   const [complete, setComplete] = useState(isActive);
-  const [edit, setEdit] = useState(() => false);
+  const [isEditing, setIsEditing] = useState(() => false);
   const inputRef = useRef(null);
 
   useEffect(() => {
     console.log("useEffect calling");
-    if (edit) inputRef.current.focus();
-  }, [edit]);
+    if (isEditing) inputRef.current.focus();
+  }, [isEditing]);
   function handleSave(e) {
     e.preventDefault();
     // Edit todo and save
@@ -17,63 +17,60 @@ export default function Todo({ todos, body, isActive, onTodosChange }) {
       return todo;
     });
     onTodosChange(updatedTodos);
-    setEdit(false);
+    setIsEditing(false);
   }
-  return (
-    <>
-      {!edit ? (
-        <div>
-          <div>
-            <label>
-              <input
-                type="checkbox"
-                checked={complete}
-                onChange={(e) => {
-                  const updatedTodos = todos.map((todo) => {
-                    if (body === todo.body)
-                      return { ...todo, active: !complete };
-                    return todo;
-                  });
-                  onTodosChange(updatedTodos);
-                  setComplete(!complete);
-                }}
-              />{" "}
-              {body}
-            </label>
-          </div>
-          <div>
-            <button onClick={() => setEdit(true)}>Edit</button>
-            <button
-              onClick={() => {
-                const remainingTodos = todos.filter(
-                  (todo) => todo.body !== body
-                );
-                onTodosChange(remainingTodos);
-              }}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      ) : (
-        <form onSubmit={handleSave}>
-          <label>New name for {body}</label>
-          <div>
-            <input
-              ref={inputRef}
-              name="newName"
-              autoComplete="off"
-              defaultValue={body}
-            />
-          </div>
-          <div>
-            <button type="button" onClick={() => setEdit(false)}>
-              Cancel
-            </button>
-            <button type="submit">Save</button>
-          </div>
-        </form>
-      )}
-    </>
+
+  const editingTemplate = (
+    <div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={complete}
+            onChange={(e) => {
+              const updatedTodos = todos.map((todo) => {
+                if (body === todo.body) return { ...todo, active: !complete };
+                return todo;
+              });
+              onTodosChange(updatedTodos);
+              setComplete(!complete);
+            }}
+          />{" "}
+          {body}
+        </label>
+      </div>
+      <div>
+        <button onClick={() => setIsEditing(true)}>Edit</button>
+        <button
+          onClick={() => {
+            const remainingTodos = todos.filter((todo) => todo.body !== body);
+            onTodosChange(remainingTodos);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
   );
+
+  const viewTemplate = (
+    <form onSubmit={handleSave}>
+      <label>New name for {body}</label>
+      <div>
+        <input
+          ref={inputRef}
+          name="newName"
+          autoComplete="off"
+          defaultValue={body}
+        />
+      </div>
+      <div>
+        <button type="button" onClick={() => setIsEditing(false)}>
+          Cancel
+        </button>
+        <button type="submit">Save</button>
+      </div>
+    </form>
+  );
+  return <>{!isEditing ? editingTemplate : viewTemplate}</>;
 }
