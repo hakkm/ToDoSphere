f9137b00d7187a37c15d8826bca876d4a4de539c
Khabir <hakim1khabir2@gmail.com>
2023-08-30 00:56:07 +0100

    Final Touch add Header Footer

--- src/Componenets/Header.js
+++ src/Componenets/Header.js
@@ -1,7 +1,6 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import PlaylistAddCheckIcon from "@mui/icons-material/PlaylistAddCheck";
-
 import Typography from "@mui/material/Typography";
 export default function Header() {
   return (
